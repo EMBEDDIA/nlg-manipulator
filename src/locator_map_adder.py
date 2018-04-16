49974d05bc3b4479f2fc5a2d83190efe786bbf7b
Leo Leppänen <leo.leppanen@helsinki.fi>
2018-04-16 12:20:50 +0300

    ENH: Integrated locator map code
    Integrated the locator map code provided by rula88
    into the pipeline. Some of the code was translated to
    python, some was modified a bit for simplicity.
    
    Co-authored-by: Rola Alhalaseh <rola.alhalaseh@helsinki.fi>

--- src/locator_map_adder.py
+++ src/locator_map_adder.py
@@ -1,40 +1,108 @@
 import logging
 import urllib.request
+import urllib.parse
 import json
+from math import cos, sqrt
 log = logging.getLogger('root')
 
 class LocatorMapAdder():
-
+	REFERENCES =[
+		{"name":"Helsinki", "latitude":60.170, "longitude":24.940},
+		{"name":"Espoo", "latitude":60.210, "longitude":24.660},
+		{"name":"Tampere", "latitude":61.520, "longitude":23.760},
+		{"name":"Turku", "latitude":60.450, "longitude":22.250},
+		{"name":"Oulu", "latitude":65.020, "longitude":25.470},
+		{"name":"Lahti", "latitude":60.990, "longitude":25.660},
+		{"name":"Kuopio", "latitude":62.900, "longitude":27.700},
+		{"name":"Jyvaskyla", "latitude":62.260, "longitude":25.750},
+		{"name":"Pori", "latitude":61.490, "longitude":21.770},
+		{"name":"Lappeenranta", "latitude":61.060, "longitude":28.180},
+		{"name":"Vaasa", "latitude":63.100, "longitude":21.610},
+		{"name":"Kotka", "latitude":60.470, "longitude":26.930},
+		{"name":"Joensuu", "latitude":62.610, "longitude":29.780},
+		{"name":"Hameenlinna", "latitude":61.000, "longitude":24.450},
+		{"name":"Porvoo", "latitude":60.390, "longitude":25.660},
+		{"name":"Mikkeli", "latitude":61.700, "longitude":27.260},
+		{"name":"Hyvinkaa", "latitude":60.640, "longitude":24.870},
+		{"name":"Rauma", "latitude":61.130, "longitude":21.520},
+		{"name":"Rovaniemi", "latitude":66.490, "longitude":25.700},
+		{"name":"Lohja", "latitude":60.250, "longitude":24.080},
+		{"name":"Kokkola", "latitude":63.840, "longitude":23.120},
+		{"name":"Kajaani", "latitude":64.230, "longitude":27.730},
+		{"name":"Kouvola", "latitude":60.880, "longitude":26.700},
+		{"name":"Imatra", "latitude":61.180, "longitude":28.760},
+		{"name":"Nurmijarvi", "latitude":60.470, "longitude":24.790},
+		{"name":"Seinajoki", "latitude":62.790, "longitude":22.840},
+		{"name":"Savonlinna", "latitude":61.870, "longitude":28.910},
+		{"name":"Nokia", "latitude":61.490, "longitude":23.480},
+		{"name":"Riihimaki", "latitude":60.740, "longitude":24.790},
+		{"name":"Vihti", "latitude":60.420, "longitude":24.330},
+		{"name":"Salo", "latitude":60.390, "longitude":23.130},
+		{"name":"Iisalmi", "latitude":63.570, "longitude":27.180},
+		{"name":"Varkaus", "latitude":62.320, "longitude":27.910},
+		{"name":"Kemi", "latitude":65.760, "longitude":24.550},
+		{"name":"Tornio", "latitude":65.860, "longitude":24.170},
+		{"name":"Kuusamo", "latitude":65.960, "longitude":29.180},
+		{"name":"Lieksa", "latitude":63.320, "longitude":30.040},
+		{"name":"Naantali", "latitude":60.470, "longitude":22.020},
+		{"name":"Hamina", "latitude":60.570, "longitude":27.210},
+	]
 	CACHE = {}
 
 	def add(self, html, location):
-		lat, lng = self._get_coords(location)
-		return self._get_map(lat, lng) + html
+		location_lat, location_lng = self._get_coords(location)
+		reference_lat, reference_lng = self._get_nearest_reference_coords(location_lat, location_lng)
+		return self._get_map(location_lat, location_lng, reference_lat, reference_lng) + html
+
+	def _distance(self, lat1, lng1, lat2, lng2):
+		x = (lng1 - lng2) * cos((lat1 + lat2) / 2)
+		y = (lat1 - lat2)
+		return sqrt(x**2 + y**2) * 6371000
+
+	def _get_nearest_reference_coords(self, lat, lng):
+		closest_reference = self.REFERENCES[0]
+		min_distance = self._distance(lat, lng, closest_reference['latitude'], closest_reference['longitude'])
+		for reference in self.REFERENCES[1:]:
+			distance = self._distance(lat, lng, reference['latitude'], reference['longitude'])
+			if distance < min_distance:
+				min_distance = distance
+				closest_reference = reference
+		return (closest_reference['latitude'], closest_reference['longitude'])
 		
 	def _get_coords(self, location):
 		if location not in self.CACHE:
-			response = urllib.request.urlopen("https://api.digitransit.fi/geocoding/v1/search?text={}&size=1".format(location)).read()
+			url = "https://api.digitransit.fi/geocoding/v1/search?text={}&size=1".format(urllib.parse.quote(location))
+			response = urllib.request.urlopen(url).read()
 			data = json.loads(response.decode('utf-8'))
-			self.CACHE[location] = data['features'][0]['geometry']['coordinates']
+			self.CACHE[location] = data['features'][0]['geometry']['coordinates'][1], data['features'][0]['geometry']['coordinates'][0]
 		return self.CACHE[location]
 
-	def _get_map(self, lat, lng):
+	def _get_map(self, location_lat, location_lng, reference_lat, reference_lng):
 		return """
-			<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
-			integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
-			crossorigin=""/>
-			<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
-			integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
-			crossorigin=""></script>
-			<div style="float:right; width:300px; height:400px;margin:15px;" id="map"></div>
+			<div style="float:right; width:300px; height:400px; margin:15px;" id="map"></div>
 			<script>
-				var map = L.map('map').setView([64.96, 27.33], 13);
-				L.tileLayer('http://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
-					attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-				}}).addTo(map);
-				map.fitBounds([[71.801, 38.408], [57.040, 12.041]])
-				L.marker([{}, {}]).addTo(map);
+			function createMap() {{
+				// Create map
+				var mapOptions= {{
+					center: new google.maps.LatLng({location_lat}, {location_lng}),
+					zoom:9,
+				}};
+				var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+				// Add markers
+				var location_marker = new google.maps.Marker({{
+					position: mapOptions.center
+				}});
+				location_marker.setMap(map);
+
+				// Set zoom
+				var bounds = new google.maps.LatLngBounds()
+				bounds.extend(location_marker.position)
+				bounds.extend(new google.maps.LatLng({reference_lat}, {reference_lng}))
+				map.fitBounds(bounds);
+			}}
 			</script>
-		""".format(lng,lat)
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7A_wqRiWrVXFAxqyzUY09oDNTxMJl9ZA&callback=createMap"></script>
+		""".format(location_lat=location_lat, location_lng=location_lng, reference_lat=reference_lat, reference_lng=reference_lng)
 
 
