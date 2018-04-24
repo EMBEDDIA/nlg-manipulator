a22a15bbae7b57796700220eeb2ccf56a08eb35a
Leo Leppänen <leo.leppanen@helsinki.fi>
2018-04-24 11:45:08 +0300

    ENH: Focus the news on more recent things

--- src/crime_importance_allocator.py
+++ src/crime_importance_allocator.py
@@ -53,8 +53,8 @@ class CrimeImportanceSelector(NLGPipelineComponent):
             # where diff is the difference between the next year (now 2019)
             # and the year the fact discusses. That is, facts regarding
             # the current year get a multiplier of 1, the year before that
-            # gets a multiplied of 0.5, the year before that 0.33... etc.
-            when_score *= min(1, (1 / (2019 - int(fact.when_2))))
+            # gets a multiplied of 0.5, the year before that 0.11... etc.
+            when_score *= min(1, (1 / (2019 - int(fact.when_2))**2))
             when_score *= 2
         elif fact.when_type == "month":
             # For months, the penalty is scaled linearly between the multiplers
@@ -64,8 +64,8 @@ class CrimeImportanceSelector(NLGPipelineComponent):
             # more newsworthy than the month 2019M12 by the same amount that
             # 2019M12 is more newsworthy than 2019M11.
             year, month = fact.when_2.split('M')
-            this_year = min(1, (1 / (2019 - int(year))))
-            prev_year = min(1, (1 / (2019 - (int(year)-1))))
+            this_year = min(1, (1 / (2019 - int(year)))**2)
+            prev_year = min(1, (1 / (2019 - (int(year)-1)))**2)
             month_effect = (this_year - prev_year) / (int(month)+1)
             when_score *= this_year - month_effect
 
