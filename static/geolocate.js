0cd1e3adf973aaf8ae20933471fc5e784ec74ec8
Leo Leppänen <leo.leppanen@helsinki.fi>
2018-05-14 14:24:34 +0300

    MINOR: Terminate some nonterminated JS lines

--- static/geolocate.js
+++ static/geolocate.js
@@ -5,11 +5,11 @@ $(document).ready(function (){
             $('#geo-wait').removeClass('hide');
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            url = 'https://api.digitransit.fi/geocoding/v1/reverse?point.lat=' + lat + '&point.lon=' + lon + '&size=1'
+            url = 'https://api.digitransit.fi/geocoding/v1/reverse?point.lat=' + lat + '&point.lon=' + lon + '&size=1';
             $.getJSON(url, function(response) {
                 city = response.features[0].properties.localadmin;
-                btn = $('#geolocate')
-                btn.attr('href', 'city?city=' + city)
+                btn = $('#geolocate');
+                btn.attr('href', 'city?city=' + city);
                 $('#geo-wait').addClass('hide');
                 btn.removeClass('disabled');
                 $('#geo-icon').removeClass('hide');
