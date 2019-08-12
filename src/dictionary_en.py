887a1fabcb50da09b605976aca6ff7a8637507bc
Rämö <miiaramo@lm7-cs-46.local>
2019-08-12 12:46:27 +0300

    new phrases for datasets

--- src/dictionary_en.py
+++ src/dictionary_en.py
@@ -1,35 +1,94 @@
-INDICATORS = {
-	'hi00' :	'all items',
-	'hi01' :	'food and non alcoholic beverages',
-	'hi02' :	'alcoholic beverages and tobacco',
-	'hi03' :	'clothing and footwear',
-	'hi04' :	'housing, water, electricity,gas and other fuels',
-	'hi05' :	'furnishings, household equipment and maintenance',
-	'hi06' :	'health',
-	'hi07' :	'transport',
-	'hi08' :	'communication',
-	'hi09'	: 'recreation and culture',
-	'hi10' :	'education',
-	'hi11' :	'hotels, cafes and restaurants',
-	'hi12' :	'miscellaneous goods and services',
-	'hi00xef' : 'all items excluding energy, food, alcohol and tobacco',
-	'hi00xtb' :	'all items excluding tobacco',
-	'hie' :	'energy',
-	'hif' :	'food',
-	'hifu' :	'unprocessed food',
-	'hig' :	'total goods',
-	'hiig' :	'industrial goods',
-	'his' :	'total services',
-	'hiigxe' :	'non-energy industrial goods',
-	'hi00xe' :	'all items excluding energy',
-	'hi00xefu' :	'all items excluding energy and unprocessed food',
-	'hi00xes' :	'all items excluding energy and seasonal food',
+# what type palojen järjestys määritelty täällä
+
+CPHI = {
+    # 'HELP': ['unit, indicator'],
+    # 'unit' : {
+        'hicp2015': 'harmonized consumer price index', # harmonized consumer price index (2015=100)
+        'rt1': 'growth rate on previous month', # growth rate on previous period (t/t-1)
+        'rt12': 'growth rate', # growth rate (t/t-12)
+    # },
+    # 'indicator': {
+        'cp-hi00': 'all items',
+        'cp-hi01':	'food and non alcoholic beverages',
+        'cp-hi02':	'alcoholic beverages and tobacco',
+        'cp-hi03':	'clothing and footwear',
+        'cp-hi04':	'housing, water, electricity,gas and other fuels',
+        'cp-hi05':	'furnishings, household equipment and maintenance',
+        'cp-hi06':	'health',
+        'cp-hi07':	'transport',
+        'cp-hi08':	'communication',
+        'cp-hi09': 'recreation and culture',
+        'cp-hi10':	'education',
+        'cp-hi11':	'hotels, cafes and restaurants',
+        'cp-hi12':	'miscellaneous goods and services',
+        'cp-hi00xef': 'all items excluding energy, food, alcohol and tobacco',
+        'cp-hi00xtb': 'all items excluding tobacco',
+        'cp-hie': 'energy',
+        'cp-hif': 'food',
+        'cp-hifu':	'unprocessed food',
+        'cp-hig': 'total goods',
+        'cp-hiig':	'industrial goods',
+        'cp-his': 'total services',
+        'cp-hiigxe': 'non-energy industrial goods',
+        'cp-hi00xe': 'all items excluding energy',
+        'cp-hi00xefu': 'all items excluding energy and unprocessed food',
+        'cp-hi00xes': 'all items excluding energy and seasonal food',
+    # },
+}
+
+INCOME = {
+    # 'sex': {
+        'f': 'among females',
+        'm': 'among males',
+        't': 'among all sexes',
+    # },
+    # 'indicator': {
+        'eur': 'euros',
+        'nac': 'national currency',
+        'pps': 'purchasing power standard',
+    # },
+    # 'unit': {
+        'med-e': 'median equivalised net income',
+        'mei-e': 'mean equivalised net income',
+    # },
+    # 'age_group': {
+        'total': 'all ages',
+        'y-lt6': 'less than 6 years',
+        'y6-10': 'from 6 to 10 years',
+        'y6-11': 'from 6 to 11 years',
+        'y11-15': 'from 11 to 15 years',
+        'y12-17': 'from 12 to 17 years',
+        'y-lT16': 'less than 16 years',
+        'y16-24': 'from 16 to 24 years',
+        'y16-64': 'from 16 to 64 years',
+        'y-ge16': '16 years or over',
+        'y-lT18': 'less than 18 years',
+        'y18-24': 'from 18 to 24 years',
+        'y18-64': 'from 18 to 64 years',
+        'y-ge18': '18 years or over',
+        'y25-49': 'from 25 to 49 years',
+        'y25-54': 'from 25 to 54 years',
+        'y50-64': 'from 50 to 64 years',
+        'y55-64': 'from 55 to 64 years',
+        'y-lT60': 'less than 60 years',
+        'y-ge60': '60 years or over',
+        'y-lT65': 'less than 65 years',
+        'y65-74': 'from 65 to 74 years',
+        'y-ge65': '65 years or over',
+        'y-lT75': 'less than 75 years',
+        'y-ge75': '75 years or over',
+    # },
+}
+
+HEALTH = {
+    'hf3': "household out-of-pocket payment",
+    'pc-che': "percentual share of total current health expenditure",
 }
 
 SMALL_ORDINALS = {
     '1': "first",
     '2': "second",
-    '3': "third",
+    '3': "tcp-hird",
     '4': "fourth",
     '5': "fifth",
     '6': "sixth",
@@ -67,4 +126,8 @@ MONTHS = {
     '10': "October",
     '11': "November",
     '12': "December",
+}
+
+COUNTRIES = {
+    'at': 'Austria',
 }
