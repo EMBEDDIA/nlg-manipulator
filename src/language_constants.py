c9a5ea0d13200010ca2618f3502f7c9b00ed1fd7
Rämö <miiaramo@lm7-cs-46.local>
2019-10-23 15:05:11 +0300

    swedish constant removed for now

--- src/language_constants.py
+++ src/language_constants.py
@@ -1,18 +1,3 @@
-pronouns = {
-    "fi": {
-        1: "hän",
-        2: "hän",
-    },
-    "en": {
-        1: "he",
-        2: "she",
-    },
-    "sv": {
-        1: "han",
-        2: "hon",
-    }
-}
-
 vocabulary = {
     "fi": {
         "default_combiner": "ja",
@@ -25,12 +10,6 @@ vocabulary = {
         "inverse_combiner": "but",
         "subord_clause_start": ", which is",
         "comparator": "than",
-    },
-    "sv": {
-        "default_combiner": "och",
-        "inverse_combiner": "men",
-        "subord_clause_start": ", som är",
-        "comparator": "än",
     },
     "de": {
         "default_combiner": "und",
@@ -50,10 +29,5 @@ errors = {
         "no-messages-for-selection": "<p>Valtteri is unable to write an article on your selection.</p>",
         "general-error": "<p>Something went wrong. Please try again later.</p>",
         "no-template": "[<i>I don't know how to express my thoughts here</i>]",
-    },
-    "sv": {
-        "no-messages-for-selection": "<p>Valtteri kan inte skriva en nyhet utgående från dina val.</p>",
-        "general-error": "<p>Något gick fel. Gör ett nytt försök om en stund.</p>",
-        "no-template": "[<i>Jag vet inte hur jag ska uttrycka mina tankar här</i>]",
     },
 }
