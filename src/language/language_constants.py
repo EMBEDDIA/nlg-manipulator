d9585fc9b2ca4be5b094509a698127aacb4ee584
Rämö <miiaramo@lm7-cs-46.local>
2019-12-16 10:12:18 +0200

    Valtteri removed from error messages

--- src/language/language_constants.py
+++ src/language/language_constants.py
@@ -25,12 +25,12 @@ vocabulary = {
 
 errors = {
     "fi": {
-        "no-messages-for-selection": "<p>Valtteri ei osaa kirjoittaa uutista valinnastasi.</p>",
+        "no-messages-for-selection": "<p>Valinnastasi ei osata kirjoittaa uutista.</p>",
         "general-error": "<p>Jotain meni vikaan. Yritäthän hetken kuluttua uudelleen.</p>",
         "no-template": "[<i>Haluaisin ilmaista jotain tässä mutten osaa</i>]",
     },
     "en": {
-        "no-messages-for-selection": "<p>Valtteri is unable to write an article on your selection.</p>",
+        "no-messages-for-selection": "<p>We are unable to write an article on your selection.</p>",
         "general-error": "<p>Something went wrong. Please try again later.</p>",
         "no-template": "[<i>I don't know how to express my thoughts here</i>]",
     },
